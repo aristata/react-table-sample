12632c76e5329cba874457ddd07001565b277ccd
장성민 <aristatait@gmail.com>
2022-07-25 17:54:05 +0900

    Feat: 검색에 블러 적용

--- components/GlobalFilter.tsx
+++ components/GlobalFilter.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAsyncDebounce } from "react-table";
 
 interface Filter {
   filter: string;
@@ -6,13 +7,26 @@ interface Filter {
 }
 
 export const GlobalFilter = ({ filter, setFilter }: Filter) => {
+  const [value, setValue] = useState(filter);
+  // const changeValue = useAsyncDebounce((value) => {
+  //   setFilter(value || undefined);
+  // }, 300);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFilter(e.target.value);
+    setValue(e.target.value);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setFilter(value);
   };
   return (
     <div>
       Search: {""}
-      <input value={filter || ""} onChange={handleChange} />
+      <input
+        type={"text"}
+        value={value || ""}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
     </div>
   );
 };
